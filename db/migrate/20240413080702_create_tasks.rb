0bd0d70f338d94ad84314ada721e473988feeed0
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-04-13 17:09:58 +0900

    初期状態のタスクの状態を追加

--- db/migrate/20240413080702_create_tasks.rb
+++ db/migrate/20240413080702_create_tasks.rb
@@ -2,7 +2,8 @@ class CreateTasks < ActiveRecord::Migration[7.1]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.boolean :completed
+      # 初期状態ではタスクは未完了
+      t.boolean :completed, default: false
 
       t.timestamps
     end
