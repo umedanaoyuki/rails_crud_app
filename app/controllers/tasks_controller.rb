896cba2a3cf60f8ee0923685a1fa55b08d59ff7b
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-04-13 21:30:11 +0900

    feat: バリデーションの追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,8 +9,11 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    @task.save
+    if @task.save
     redirect_to root_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   private
